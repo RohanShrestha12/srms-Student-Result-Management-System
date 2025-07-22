5f4c9246fc9c2d5cab6ddcf9eb74095b0e8c99d3
bishalxrauniyar <Bishal.rauniyar@gmail.com>
2025-07-22 16:58:24 +0545

    added db name check config db

--- script/db/config.php
+++ script/db/config.php
@@ -1,11 +1,9 @@
 <?php
-DEFINE('DBHost','localhost');
+DEFINE('DBHost', 'localhost');
 DEFINE('DBUser', 'root');
-DEFINE('DBPass','');
-DEFINE('DBName','srms_makumbusho');
-DEFINE('DBCharset','utf8mb4');
+DEFINE('DBPass', '');
+DEFINE('DBName', 'srms_bishal_rohan');
+DEFINE('DBCharset', 'utf8mb4');
 DEFINE('DBCollation', 'utf8_general_ci');
 DEFINE('DBPrefix', '');
-
-date_default_timezone_set('Africa/Dar_es_Salaam');
-?>
+date_default_timezone_set('Asia/Kathmandu');
