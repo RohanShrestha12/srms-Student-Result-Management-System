dbff3daad8bfcfe41d7280744346d356024f3377
RohanShrestha12 <bca210604_rohan@achsnepal.edu.np>
2025-07-23 10:14:42 +0545

    save result updated

--- script/academic/save_pdf.php
+++ script/academic/save_pdf.php
@@ -70,7 +70,7 @@ $pdf->Ln(3);
 // Grade Sheet Header
 $html = '
 <div style="font-size:11px; font-weight:bold; font-family:helvetica; text-align:left; width:0cm;">
-  THE FOLLOWING ARE THE GRADE(S) SECURED BY: ' . $value[1] . '
+  THE FOLLOWING ARE THE GRADE(S) SECURED BY: '  '
   <div style="border-bottom:1px solid black; width:-1cm; margin-top:2px;"></div>
   <br>
   DATE OF BIRTH: ' . $dob_bs . ' B.S. SYMBOL NO.: ' . $symbol_no . ' GRADE XI IN THE <br>
